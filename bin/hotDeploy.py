08497372fdbaa2b32cdefd9608961828f40819cb
demo <demo@192.168.122.29>
2014-06-18 17:02:12 +0200

    Version 0.0.3

--- bin/hotDeploy.py
+++ bin/hotDeploy.py
@@ -1,7 +1,7 @@
 # enable apps hot deployment in WAS ND
 import os;
 
-dir = os.environ["OPENSHIFT_REPO_DIR"] + "deployments/"
+dir = os.environ["OPENSHIFT_REPO_DIR"] + "deployments"
 print 'Enabling hot deployment dir at ' + dir 
 c1 = AdminConfig.getid('/Cell:127Node01Cell')
 md = AdminConfig.showAttribute(c1, "monitoredDirectoryDeployment")
