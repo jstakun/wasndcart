c6c6159c541d37690bbc98d74206a6272e2352ae
demo <demo@192.168.122.29>
2014-06-20 13:07:15 +0200

    Fixed db script

--- bin/addPsqlVars.py
+++ bin/addPsqlVars.py
@@ -1,29 +1,28 @@
 import os;
 
-def variable(varname, vardesc):
-    
-    varvalue = os.environ[vaname]
-  
- 	cellName = '127Node01Cell'
- 	nodeName = '127Node01'
- 	varMapserver = AdminConfig.getid('/Cell:'+ cellName +'/Node:'+ nodeName +'/VariableMap:/')
+def variable(varname, varvalue, vardesc):
+      
+    cellName = '127Node01Cell'
+    nodeName = '127Node01'
+    varMapserver = AdminConfig.getid('/Cell:'+ cellName +'/Node:'+ nodeName +'/VariableMap:/')
 
- 	nameattr1 = ['symbolicName', varname]
- 	valattr1 = ['value', varvalue]
- 	desc1 = ["description", vardesc]
- 	attr1 = [nameattr1, valattr1, desc1]
- 	attrs1 = [attr1]
+    nameattr1 = ['symbolicName', varname]
+    valattr1 = ['value', varvalue]
+    desc1 = ["description", vardesc]
+    attr1 = [nameattr1, valattr1, desc1]
+    attrs1 = [attr1]
 
- 	entries1 = ['entries', attrs1 ]
+    entries1 = ['entries', attrs1 ]
 
- 	print "creating new variable " + varname
+    print "creating new variable " + varname
 
- 	AdminConfig.modify(varMapserver, [entries1] )
+    AdminConfig.modify(varMapserver, [entries1] )
 
- 	AdminConfig.save()
+    AdminConfig.save()
 
-variable('OPENSHIFT_POSTGRESQL_DB_HOST', 'PostgreSQL host name')
-variable('OPENSHIFT_POSTGRESQL_DB_PASSWORD', 'PostgreSQL password')
-variable('OPENSHIFT_POSTGRESQL_USERNAME', 'PostgreSQL user name')
-variable('OPENSHIFT_POSTGRESQL_DB_PORT', 'PostgreSQL port')
-variable('OPENSHIFT_POSTGRESQL_DB_NAME', 'PostgreSQL database name')
+variable('OPENSHIFT_POSTGRESQL_DB_HOST', os.environ["OPENSHIFT_POSTGRESQL_DB_HOST"], 'PostgreSQL host name')
+variable('OPENSHIFT_POSTGRESQL_DB_PORT', os.environ["OPENSHIFT_POSTGRESQL_DB_PORT"], 'PostgreSQL port')
+variable('OPENSHIFT_POSTGRESQL_DB_NAME', os.environ["OPENSHIFT_GEAR_NAME"], 'PostgreSQL database name')
+variable('OPENSHIFT_POSTGRESQL_DB_PASSWORD', os.environ["OPENSHIFT_POSTGRESQL_DB_PASSWORD"], 'PostgreSQL password')
+variable('OPENSHIFT_POSTGRESQL_DB_USERNAME', os.environ["OPENSHIFT_POSTGRESQL_DB_USERNAME"], 'PostgreSQL user name')
+
